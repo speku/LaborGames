39221e29389b223c3724f6b1cf3853020259b657
speku <mroeck@stud.hs-heilbronn.de>
2016-04-25 17:03:18 +0200

    - creating a prototype skeleton according to the tutorial on http://catlikecoding.com/unity/tutorials/runner/ - once basic elements are implemented, game logic and design will be adapted to our own specifications

---
+++ Runner/Assets/Runner/Runner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Runner : MonoBehaviour {
+
+    public static float distanceTraveled;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(5f * Time.deltaTime, 0f, 0f);
+        distanceTraveled = transform.localPosition.x;
+	}
+}
---
+++ Runner/Assets/Skyline/SkylineManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SkylineManager : MonoBehaviour {
+
+    public Transform prefab;
+    public int numberOfObjects;
+    public Vector3 startPosition;
+    public float recycleOffset;
+    public Vector3 minSize, maxSize;
+
+    private Vector3 nextPosition;
+    private Queue<Transform> objectQueue;
+
+	// Use this for initialization
+	void Start () {
+        objectQueue = new Queue<Transform>(numberOfObjects);
+        for (int i = 0; i < numberOfObjects; i++)
+        {
+            objectQueue.Enqueue((Transform)Instantiate(prefab));
+        }
+        nextPosition = startPosition;
+        for (int i = 0; i < numberOfObjects; i++)
+        {
+            Recycle();
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (objectQueue.Peek().localPosition.x + recycleOffset < Runner.distanceTraveled)
+        {
+            Recycle();
+        }
+	}
+
+    private void Recycle()
+    {
+        Vector3 scale = new Vector3(
+            Random.Range(minSize.x, maxSize.x),
+            Random.Range(minSize.y, maxSize.y),
+            Random.Range(minSize.z, maxSize.z));
+
+        Vector3 position = nextPosition;
+        position.x += scale.x * 0.5f;
+        position.y += scale.y * 0.5f;
+
+        Transform o = objectQueue.Dequeue();
+        o.localScale = scale;
+        o.localPosition = position;
+        nextPosition.x += scale.x;
+        objectQueue.Enqueue(o);
+    }
+}
